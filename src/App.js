e52f76894ac755fa2ef4b1eb313b3a0f8e16f14f
karinamulk <karinamulk@yandex.ru>
2023-03-31 13:50:33 +0300

    first commit and rules added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import RulesScreen from './pages/Rules';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    
+    <div>
+      <RulesScreen/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
